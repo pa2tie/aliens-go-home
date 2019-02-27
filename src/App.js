da3672818ab813d3db163359726382ed725a1248
pa2tie <myadsagain@gmail.com>
2019-02-28 01:19:44 +0400

    Creating the Canvas React Component. Creating the Sky React Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import Canvas from "./components/Canvas";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>{this.props.message}</h1>
-      </div>
-    );
+    return <Canvas />;
   }
 }
-
-App.propTypes = {
-  message: PropTypes.string.isRequired
-};
 
 export default App;
